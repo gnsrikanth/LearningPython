45f45c61d647356b57ebafd3b1013a76332aa0ed
Srikanth Gundavarapu <Srikanth.Gundavarapu@arup.com>
2024-02-29 14:45:18 +0530

    comitting to remote

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -2,5 +2,13 @@
 # Example file for HelloWorld
 # LinkedIn Learning Python course by Joe Marini
 #
+def main():
+    print("Hello world")
+    name = input("what is your name?")
+    print("nice to meet you",name)
+
+if __name__ == "__main__":
+    main()
+
 
 
